0dfab35fd913952abf89db49007f2857e3b83856
Nandini390 <Shara.nandini123@gmail.com>
2026-04-02 15:09:05 +0530

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='p-10'>
+      <h1 className="text-3xl font-bold">Hello, auth frontend app</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
